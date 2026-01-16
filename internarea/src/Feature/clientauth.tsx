5dc482b07ede2bfe1f532c91521697062d4b129a
BALLA KOUSHIK <koushikballa09@gmail.com>
2026-01-16 14:02:45 +0530

    update clientauth

--- internarea/src/Feature/clientauth.tsx
+++ internarea/src/Feature/clientauth.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useState, useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect, JSX } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 
-export function ClientAuth({ children }: { children: (user: any) => JSX.Element }) {
-  const [user, setUser] = useState<any>(null);
+export function ClientAuth({ children }: { children: (user: User | null) => JSX.Element }) {
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (fbUser) => setUser(fbUser));
+    const unsub = onAuthStateChanged(auth, (fbUser: User | null) => setUser(fbUser));
     return () => unsub();
   }, []);
 
