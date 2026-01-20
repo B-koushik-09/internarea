12a5552ddf7b53cbe70d69b78b6b7e66b539166a
BALLA KOUSHIK <koushikballa09@gmail.com>
2026-01-20 19:43:21 +0530

    Added new features

--- internarea/src/firebase/firebase.js
+++ internarea/src/firebase/firebase.js
@@ -1,22 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbYTYs_oSK2rvtzHxBCPFrqdXM-l7MokQ",
-  authDomain: "internshala-9e300.firebaseapp.com",
-  projectId: "internshala-9e300",
-  storageBucket: "internshala-9e300.firebasestorage.app",
-  messagingSenderId: "304425115290",
-  appId: "1:304425115290:web:1b0653f1acd4192b848923",
+  apiKey: "AIzaSyBSl2aTE4JdKS9nHHDh9R_KQvZCA6MbqW4",
+  authDomain: "internarea-ad371.firebaseapp.com",
+  projectId: "internarea-ad371",
+  storageBucket: "internarea-ad371.firebasestorage.app",
+  messagingSenderId: "877439705716",
+  appId: "1:877439705716:web:d4e9dddeaac8f129906721",
+  measurementId: "G-E9XL1DTMLP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
 
 // Enforce local persistence
 import { setPersistence, browserLocalPersistence } from "firebase/auth";
@@ -25,3 +27,5 @@ setPersistence(auth, browserLocalPersistence).catch(error => {
 });
 
 export { auth, provider };
+
+
